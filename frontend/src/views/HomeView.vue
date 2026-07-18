576eb38d20b383e3f90f299cbc4ac61de1c34985
HamadAlsayed <hamad_sayed@outlook.com>
2026-07-18 20:12:54 +0300

    feat(frontend): add card section to HomeView
    This adds a cards section, with a placeholder example, below the hero section.

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,7 +1,16 @@
+<script setup>
+import Card from '../components/Card.vue'
+</script>
+
 <template>
   <section
     class="bg-[url('../images/hero.jpg')] bg-cover bg-center min-h-[500px] flex items-center justify-center py-20 px-6">
     <h1 class="font-bold mb-4 text-4xl text-white drop-shadow-black drop-shadow-lg">This is a hero section</h1>
+  </section>
+
+  <section class="bg-gray-700 min-h-200 px-6 py-8">
+    <Card icon="" title="Course 1" description="course 1 description" />
+
   </section>
 
 </template>
