90a4ad534c38cd0e3c2c54726683e0dd6d89c630
HamadAlsayed <hamad_sayed@outlook.com>
2026-07-18 20:06:31 +0300

    feat(component): add Card.vue
    This adds a component Card.vue that accepts 3 parameters; icon, title, and description.
    - icon: only accepts paths

---
+++ frontend/src/components/Card.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="bg-gray-800 px-8 py-8 max-w-[200px]">
+        <img :src="icon" :alt="title" class="w-12 h-12">
+        <div class="text-bold text-white">{{ title }}</div>
+        <p class="text-gray-400">{{ description }}</p>
+    </div>
+</template>
+
+<script setup>
+defineProps(['title', 'description', 'icon']);
+</script>
