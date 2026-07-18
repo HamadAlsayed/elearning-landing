ffe16a81b2c19eafe4796d0ecf1efdb3c6ebebc6
HamadAlsayed <hamad_sayed@outlook.com>
2026-07-18 19:03:30 +0300

    style: set background image for hero in home page

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,5 +1,7 @@
 <template>
-  <section class="bg-gray-600 py-50 px-6">
-    <h1 class="font-bold mb-4 text-4xl">This is a hero section</h1>
+  <section
+    class="bg-[url('../images/hero.jpg')] bg-cover bg-center min-h-[500px] flex items-center justify-center py-20 px-6">
+    <h1 class="font-bold mb-4 text-4xl text-white drop-shadow-black drop-shadow-lg">This is a hero section</h1>
   </section>
+
 </template>
